8de26375204d5ec8f627fe136dd6537e69ae10f0
PJZ9n <pjz9n@yahoo.co.jp>
2020-04-30 08:41:29 +0900

    設定を実装

--- src/PJZ9n/QuitMessage/Main.php
+++ src/PJZ9n/QuitMessage/Main.php
@@ -23,9 +23,17 @@ declare(strict_types=1);
 
 namespace PJZ9n\QuitMessage;
 
+use jojoe77777\FormAPI\CustomForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\permission\Permission;
+use pocketmine\permission\PermissionManager;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener
 {
@@ -33,8 +41,56 @@ class Main extends PluginBase implements Listener
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
+        $this->reloadConfig();
         
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        
+        PermissionManager::getInstance()->addPermission(new Permission(
+            "quitmessage.command.quitmessage",
+            null,
+            Permission::DEFAULT_OP
+        ));
+        
+        $command = new PluginCommand("quitmessage", $this);
+        $command->setUsage("/quitmessage");
+        $command->setDescription("Quit時に送信するメッセージを設定する");
+        $command->setAliases([
+            "qm",
+        ]);
+        $command->setPermission("quitmessage.command.quitmessage");
+        $this->getServer()->getCommandMap()->register("quitmessage", $command);
+    }
+    
+    public function onDisable(): void
+    {
+        $this->saveConfig();//Configをセーブ
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        //プレイヤーか確認
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(TextFormat::RED . "このコマンドはプレイヤーから実行してください。");
+            return true;
+        }
+        
+        //フォーム作成
+        $form = new CustomForm(function (Player $player, $data): void {
+            //受信したときの処理
+            if ($data === null) {
+                //フォームが閉じられた
+                return;//ここで終了
+            }
+            
+            $message = $data[0];
+            $this->getConfig()->set("message", $message);
+            $player->sendMessage("メッセージを {$message} に設定しました！");
+        });
+        $form->setTitle("Quit時のメッセージ設定");
+        $form->addInput("Quit時のメッセージ", "ここに入力してください", strval($this->getConfig()->get("message")));
+        $sender->sendForm($form);
+        
+        return true;
     }
     
     /**
@@ -46,6 +102,7 @@ class Main extends PluginBase implements Listener
     public function onPlayerQuit(PlayerQuitEvent $event): void
     {
         $message = $this->getConfig()->get("message");
+        $message = str_replace("{name}", $event->getPlayer()->getName(), $message);
         $this->getServer()->broadcastMessage($message);
     }
     
